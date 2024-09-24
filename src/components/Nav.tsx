e94df4199449323c339c2de247bd169c33d9ed00
hector3211e <hectororopesa5@gmail.com>
2024-09-24 17:42:02 -0400

    upate

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,7 +3,7 @@ import { Button } from "./ui/button";
 
 export default function NavBar() {
   return (
-    <nav className="flex justify-between p-2">
+    <nav className="absolute top-0 inset-x-0 z-50 flex justify-between p-2">
       <div className="flex items-center space-x-1">
         <img width={30} src={"/honey_logo.png"} alt="honey-astrology-logo" />
         <h3 className="font-bold text-xl bg-gradient-to-br from-secondary via-primary to-accent text-transparent bg-clip-text">
@@ -11,13 +11,13 @@ export default function NavBar() {
         </h3>
       </div>
       <div className="flex items-center space-x-3">
-        <a href="#">
+        <a href="/">
           <Button variant={"link"} className="flex items-center space-x-2">
             <House className="size-4" />
             <p>Home</p>
           </Button>
         </a>
-        <a href="#">
+        <a href="#about">
           <Button
             variant={"link"}
             className="flex justify-center items-center space-x-1"
@@ -26,10 +26,10 @@ export default function NavBar() {
             <p>About</p>
           </Button>
         </a>
-        <a href="#">
+        <a href="#contact">
           <Button variant={"link"} className="flex items-center space-x-2">
             <SquareUser className="size-4" />
-            <p>Blog</p>
+            <p>Contact</p>
           </Button>
         </a>
       </div>
