f68f03140af198c357e60773b30425fa00e832a3
hector3211e <hectororopesa5@gmail.com>
2024-11-14 21:48:17 -0500

    update: text

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -20,13 +20,14 @@ export default function Navbar() {
   ];
 
   return (
-    <nav className="bg-background z-50">
+    <nav className="z-50 p-2">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <a href="/" className="flex items-center flex-shrink-0">
               <img
                 width={40}
+                height={40}
                 src={"/honey_logo.png"}
                 alt="honey-astrology-logo"
               />
